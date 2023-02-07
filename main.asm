01846fd7df44c3ba6d31e5c7183546961a30385b
London <stlondon@uiowa.edu>
2023-02-07 17:59:42 -0600

    added push button functionality with hardware debouncing

--- main.asm
+++ main.asm
@@ -11,6 +11,7 @@
 sbi DDRB,0
 sbi DDRB,1
 sbi DDRB,2
+cbi DDRB,3
 
 start:
 ;0 = 3F
@@ -23,7 +24,9 @@ start:
 ;7 = 07
 ;8 = 7F
 ;9 = 67
-ldi R16, 0x67 ; load pattern to display
+ldi R16, 0x3F ; load pattern to display
+SBIC PINB,3
+ldi R16, 0x66
 rcall display
 display: 
 	; backup used registers on stack
@@ -58,3 +61,22 @@ end:
 	pop R17
 	pop R16
 	ret 
+
+
+.equ count = 0x6B33			; assign a 16-bit value to symbol "count"
+
+delay_long:
+	ldi r30, low(count)	;1  	; r31:r30  <-- load a 16-bit value into counter register for outer loop
+	ldi r31, high(count);1
+d1:
+	ldi   r29, 0x4E	;1	    	; r29 <-- load a 8-bit value into counter register for inner loop
+d2:
+	nop	;1							; no operation
+	dec   r29  ;1          		; r29 <-- r29 - 1
+	brne  d2	;1							; branch to d2 if result is not "0"
+	sbiw r31:r30, 1		;2			; r31:r30 <-- r31:r30 - 1
+	brne d1		;1							; branch to d1 if result is not "0"
+	ret		;4									; return
+
+
+.exit
